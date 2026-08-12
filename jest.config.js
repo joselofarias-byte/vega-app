77adeb3e44c2193d21a2aff7b231718aa86eed6f
joselofarias-byte <289238002+joselofarias-byte@users.noreply.github.com>
2026-08-12 17:32:28 -0300

    test: isolate linear gradient native view in Jest

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,8 @@ module.exports = {
     '^@expo/vector-icons(?:/.*)?$': '<rootDir>/__mocks__/expo-vector-icons.js',
     '^react-native-mmkv-storage$':
       '<rootDir>/__mocks__/react-native-mmkv-storage.js',
+    '^react-native-linear-gradient$':
+      '<rootDir>/__mocks__/react-native-linear-gradient.js',
     '\\.(css|less|scss|sass)$':
       '<rootDir>/__mocks__/expo-ui-jetpack-compose-modifiers.js',
   },
