482088db129ef7ad1af15edf6b2b98c16eb83f27
joselofarias-byte <289238002+joselofarias-byte@users.noreply.github.com>
2026-08-12 17:14:06 -0300

    test: transform RN ESM dependencies and mock styles

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@expo|expo|react-native-image-colors|react-native-markdown-display)/)',
+  ],
   moduleNameMapper: {
     '^@expo/ui/jetpack-compose$':
       '<rootDir>/__mocks__/expo-ui-jetpack-compose.js',
     '^@expo/ui/jetpack-compose/modifiers$':
       '<rootDir>/__mocks__/expo-ui-jetpack-compose-modifiers.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
   },
 };
