c54ff7e262ec98469a54a155afed74a065cb8e13
joselofarias-byte <289238002+joselofarias-byte@users.noreply.github.com>
2026-08-12 17:39:43 -0300

    test: use local native-free Reanimated facade

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: '@react-native/jest-preset',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation/.*|@expo/.*|expo(?:-.*)?|react-native-reanimated|react-native-worklets|react-native-image-colors|react-native-markdown-display)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation/.*|@expo/.*|expo(?:-.*)?|react-native-image-colors|react-native-markdown-display)/)',
   ],
   moduleNameMapper: {
     '^@expo/ui/jetpack-compose$':
@@ -13,7 +13,8 @@ module.exports = {
       '<rootDir>/__mocks__/react-native-mmkv-storage.js',
     '^react-native-linear-gradient$':
       '<rootDir>/__mocks__/react-native-linear-gradient.js',
-    '^react-native-reanimated$': 'react-native-reanimated/mock',
+    '^react-native-reanimated$':
+      '<rootDir>/__mocks__/react-native-reanimated.js',
     '\\.(css|less|scss|sass)$':
       '<rootDir>/__mocks__/expo-ui-jetpack-compose-modifiers.js',
   },
